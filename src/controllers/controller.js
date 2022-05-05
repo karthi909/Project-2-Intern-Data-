6fe79786e3a2bf7333a62867e981eed991356122
karthi909 <karthikeyareddy440@gmail.com>
2022-05-05 12:54:58 +0530

    done

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -7,9 +7,13 @@ const mongoose = require("mongoose")
 const createCollage = async (req, res) => {
     try{
         let data = req.body
-        if(Object.keys(data).length == 0) return res.status(400).send({msg:"data is Missing"})
-        if(data.name.trim().length == 0) return res.status(400).send({msg:"name is Required"})
-        if(data.fullName.trim().length == 0) return res.status(400).send({msg:"fullName is Required"})
+        if(Object.keys(data).length == 0) return res.status(400).send({status: false, msg:"data is Missing"})
+        if(data.name.trim().length == 0) return res.status(400).send({status:false, msg:"name is Required"})
+        if(data.fullName.trim().length == 0) return res.status(400).send({status: false, msg:"fullName is Required"})
+        if(data.logoLink.trim().length == 0) return res.status(400).send({status: false, msg:'LogoLink is Required'})
+
+        let getUniqueValues = await collageModel.findOne({name: data.name  });
+        if (getUniqueValues) return res.status(400).send({ status: false, message: `${data.name} already exist` })
 
 
         let savedData = await collageModel.create(data)
@@ -23,23 +27,25 @@ const createCollage = async (req, res) => {
 const createIntern = async (req, res) => {
     try{
         let data = req.body
-        if(Object.keys(data).length == 0) return res.status(400).send({msg:"data is Missing"})
-        if(data.name.trim().length == 0) return res.status(400).send({msg:"name is Required"})
-        if(data.email.trim().length == 0) return res.status(400).send({msg:"email is Requried"})
-        if(data.mobile.trim().length == 0 ) return res.status(400).send({msg:"mobile Number is Requried"})
+        if(Object.keys(data).length == 0) return res.status(400).send({status: false,msg:"data is Missing"})
+        if(data.name.trim().length == 0) return res.status(400).send({status: false, msg:"name is Required"})
+        if(!data.email) return res.status(400).send({status: false, msg:"email is Requried"})
+        if(!data.mobile) return res.status(400).send({status: false, msg:"mobile Number is Requried"})
 
-        let collageid = req.body.collageId   
+        let collageid = req.body.collegeId   
         if (collageid.trim().length == 0) return res.send({ status: false, Error: 'collage Id is missing' })
         if (!mongoose.isValidObjectId(collageid)) return res.status(404).send({ status: false, Error: "Invalid Mongoose object Id" }) 
 
         let collage = await collageModel.findOne({ _id: collageid }, { _id: 1 }); 
-       
         if(collage == null || undefined) return res.status(400).send({msg:"not a valid Id"})
 
 
       
-        let getUniqueValues = await internModel.findOne({ $or: [{ email: data.email }, { mobile: data.mobile }] });
-        if (getUniqueValues) return res.status(400).send({ status: false, message: "Email or Mobile number already exist" })
+        let getUniqueValues = await internModel.findOne({ mobile: data.mobile });
+        if (getUniqueValues) return res.status(400).send({ status: false, message: `Mobile_number- ${data.mobile} already existing in the data` })
+
+        let getUniqueEmail = await internModel.findOne({email: data.email});
+        if(getUniqueEmail) return res.status(400).send({status: false, msg: `email- "${data.email}" already existing in data `})
         
 
         let savedData = await internModel.create(data)
